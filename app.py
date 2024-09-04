a429b3d8e2ccff7cf2dd76d5f44852751886b47d
MridulS <mriduls512@gmail.com>
2024-09-05 03:38:25 +0530

    done

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, send_file
+import qrcode
+import io
+from qrcode.image.pure import PyPNGImage
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate_qr', methods=['POST'])
+def generate_qr():
+    link = request.form['link']  # Corrected the way form data is accessed
+    qr = qrcode.QRCode(  # Corrected the variable name to `qr`
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=4
+    )
+    qr.add_data(link)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img_io = io.BytesIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
